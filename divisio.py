0a264e8cdbf5c756a5e2e39afc271009e2158ede
vbernethola <vbernet@instituticaria.cat>
2025-10-20 08:09:04 +0200

    ggg

---
+++ divisio.py
@@ -0,0 +1,9 @@
+dividendo = float(input("Dime el dividendo: "))
+divisor = float(input("Entra el divisor: "))
+
+cociente = dividendo // divisor
+residuo = dividendo % divisor
+
+print(f"Divisió: {dividendo} / {divisor}")
+print(f"Quocient: {cociente}")
+print(f"Residu: {residuo}")
