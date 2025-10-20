cef8a297315f261699bc24e3499e9206a0406f5e
vbernethola <vbernet@instituticaria.cat>
2025-10-20 08:34:03 +0200

    meganigger

--- divisio.py
+++ divisio.py
@@ -1,5 +1,5 @@
-dividendo = input("Dime el dividendo: ")
-divisor = input("Entra el divisor: ")
+dividendo = int(input("Dime el dividendo: "))
+divisor = int(input("Entra el divisor: "))
 
 cociente = dividendo // divisor
 residuo = dividendo % divisor
